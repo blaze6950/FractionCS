e5666fbd5ef079e4ab8e47ffab5b8232fe2df368
User <User@WIN-GQOKRARBRRP>
2018-08-13 12:34:25 +0300

    Add project files.

---
+++ FractionCS/Fraction.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Numerics;
+
+namespace FractionCS
+{
+    /*
+     public static тип operator знак(параметры);
+     
+     */
+    class Fraction
+    {
+        public int Numerator { get; set; }
+
+        int _denominator = 1;
+        public int Denominator
+        {
+            get { return _denominator; }
+            set 
+            {
+                if (value == 0)
+                    throw new DivideByZeroException();
+
+                _denominator = value; 
+            }          
+        }
+
+        public Fraction(){}
+
+        public Fraction(int numerator, int denominator)
+        {
+            if (denominator == 0)
+                throw new DivideByZeroException();
+ 
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public void Optimization()
+        {
+            int ccd = (Int32)BigInteger.GreatestCommonDivisor(Numerator, Denominator);
+            Numerator /= ccd;
+            Denominator /= ccd;
+        }
+
+        public static Fraction operator- (Fraction fr)
+        {
+            Fraction res = new Fraction();
+            res.Numerator = -fr.Numerator;
+            res.Denominator = fr.Denominator;
+
+            return res;
+        }
+
+        public static Fraction operator* (Fraction op1, Fraction op2)
+        {
+            Fraction res = new Fraction();
+            res.Numerator = op1.Numerator * op2.Numerator;
+            res.Denominator = op1.Denominator * op2.Denominator;
+            res.Optimization();
+            return res;
+        }
+
+        public static Fraction operator *(Fraction op1, int op2)
+        {
+            Fraction res = new Fraction();
+            res.Numerator = op1.Numerator * op2;
+            res.Denominator = op1.Denominator;
+
+            res.Optimization();
+            return res;
+        }
+
+        public static Fraction operator *(int op1, Fraction op2)
+        {
+            Fraction res = new Fraction();
+            res.Numerator = op1 * op2.Numerator;
+            res.Denominator = op2.Denominator;
+
+            res.Optimization();
+            return res;
+        }
+
+        public static Fraction operator /(Fraction op1, Fraction op2)
+        {
+            Fraction res = new Fraction();
+            res.Numerator = op1.Numerator * op2.Denominator;
+            res.Denominator = op1.Denominator * op2.Numerator;
+            res.Optimization();
+            return res;
+        }
+
+        public static Fraction operator /(Fraction op1, int op2)
+        {
+            Fraction res = new Fraction();
+            res.Numerator = op1.Numerator;
+            res.Denominator = op1.Denominator * op2;
+
+            res.Optimization();
+            return res;
+        }
+
+        public static Fraction operator /(int op1, Fraction op2)
+        {
+            Fraction res = new Fraction();
+            res.Numerator = op2.Numerator;
+            res.Denominator = op1 * op2.Denominator;
+
+            res.Optimization();
+            return res;
+        }
+
+        public static bool operator >(Fraction op1, Fraction op2)
+        {
+            if (op1 == null || op2 == null)
+            {
+                return false;
+            }
+            return op1.Numerator > op2.Numerator && op1.Denominator > op2.Denominator;
+        }
+
+        public static bool operator <(Fraction op1, Fraction op2)
+        {
+            if (op1 == null || op2 == null)
+            {
+                return false;
+            }
+            return op1.Numerator < op2.Numerator && op1.Denominator < op2.Denominator;
+        }
+
+        public static bool operator==(Fraction op1, Fraction op2)
+        {
+            return op2 != null && (op1 != null && (op1.Numerator == op2.Numerator
+                                                   && op1.Denominator == op2.Denominator));
+        }
+
+        public static bool operator <=(Fraction op1, Fraction op2)
+        {
+            if (op1 == null || op2 == null)
+            {
+                return false;
+            }
+            return op1.Numerator == op2.Numerator && op1.Denominator == op2.Denominator || op1.Numerator < op2.Numerator && op1.Denominator < op2.Denominator;
+        }
+
+        public static bool operator >=(Fraction op1, Fraction op2)
+        {
+            if (op1 == null || op2 == null)
+            {
+                return false;
+            }
+            return op1.Numerator == op2.Numerator && op1.Denominator == op2.Denominator || op1.Numerator > op2.Numerator && op1.Denominator > op2.Denominator;
+        }
+
+        public static bool operator !=(Fraction op1, Fraction op2)
+        {
+            return op2 != null && (op1 != null && (op1.Numerator != op2.Numerator
+                                                   || op1.Denominator != op2.Denominator));
+        }
+
+        public static Fraction operator++ (Fraction op)
+        {
+            Fraction res = new Fraction();
+            res.Numerator = op.Numerator + op.Denominator;
+            res.Denominator = op.Denominator;
+
+            res.Optimization();
+            return res;
+        }
+
+        public static Fraction operator --(Fraction op)
+        {
+            Fraction res = new Fraction();
+            res.Numerator = op.Numerator - op.Denominator;
+            res.Denominator = op.Denominator;
+
+            res.Optimization();
+            return res;
+        }
+
+        public static bool operator true(Fraction f)
+        {
+            return f.Numerator != 0;
+        }
+
+        public static bool operator false(Fraction f)
+        {
+            return f.Numerator == 0;
+        }
+
+
+        //public static explicit | implicit operator type_dest(type_src name)
+        public static explicit operator int (Fraction f)
+        {
+            return f.Numerator / f.Denominator;
+        }
+
+        public static implicit operator Fraction(int num)
+        {
+            return new Fraction(num, 1);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} / {1}", Numerator, Denominator);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Fraction fr = obj as Fraction;
+            if (obj == null || fr == null )
+                return false;
+
+
+            return Numerator == fr.Numerator
+                && Denominator == fr.Denominator;
+        }
+
+        // ReSharper disable once NonReadonlyMemberInGetHashCode
+        public override int GetHashCode() => Numerator ^ Denominator;
+    }
+}
---
+++ FractionCS/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+
+namespace FractionCS
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            
+            Fraction fr1 = new Fraction(1, 2);
+            Console.WriteLine("{0} / {1}" , fr1.Numerator, fr1.Denominator);
+
+            try
+            {
+                Fraction fr2 = new Fraction(2, 0);
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Fraction fr3 = -fr1;
+            Console.WriteLine("{0} / {1}", fr3.Numerator, fr3.Denominator);
+
+            fr3.Numerator = 1;
+            fr3.Denominator = 3;
+
+            Fraction fr4 = fr1 * fr3;
+            Console.WriteLine("{0} / {1}", fr4.Numerator, fr4.Denominator);
+
+            fr4 = fr1 * 3;
+            Console.WriteLine("{0} / {1}", fr4.Numerator, fr4.Denominator);
+
+            fr4 = 3 * fr3;
+            Console.WriteLine("{0} / {1}", fr4.Numerator, fr4.Denominator);
+
+            fr3 *= 3;
+            Console.WriteLine("f3 = {0} / {1}", fr3.Numerator, fr3.Denominator);
+
+            if(fr4 != fr3)
+            {
+                Console.WriteLine("fr4 != fr3");
+            }
+
+            Console.WriteLine("Operator ++");
+            Console.WriteLine("fr1 = {0} / {1}", fr1.Numerator, fr1.Denominator);
+            fr4 = fr1++;
+            Console.WriteLine("fr1 = {0} / {1}", fr1.Numerator, fr1.Denominator);
+            Console.WriteLine("fr4 = {0} / {1}", fr4.Numerator, fr4.Denominator);
+
+            if(fr4)
+            {
+                Console.WriteLine("fr4 is true");
+            }
+            else 
+            {
+                Console.WriteLine("fr4 is false");
+            }
+
+            int a = (int)fr4;
+            Console.WriteLine("fr4 to int => {0}", a);
+
+            Fraction fr5 = 5;
+            Console.WriteLine("fr5 = {0}", fr5); //fr5.ToString();
+            Console.WriteLine(fr5.ToString());
+        
+        }
+    }
+}
